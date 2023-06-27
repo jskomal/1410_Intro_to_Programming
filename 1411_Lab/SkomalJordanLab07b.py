a0276059ee26ee04124005d81d74104bf97cf201
Jordan Skomal <jskomal@gmail.com>
2023-06-27 13:26:44 -0600

    6/27 avg work

---
+++ 1411_Lab/SkomalJordanLab07b.py
@@ -0,0 +1,16 @@
+def get_avg_score():
+    score_file = open("score.txt", "r")
+    outfile_name = input("Please enter a name for the output file: ")
+    outfile = open(outfile_name, "w")
+    for line in score_file:
+        temp_split = line.split()
+        uname = temp_split[0]
+        scores = list(map(float, temp_split[1:-1]))
+        avg = sum(scores) / len(scores)
+        print(f"{uname} {avg}")
+        print(f"{uname} {avg}", file=outfile)
+
+    score_file.close()
+
+
+get_avg_score()
--- wk4/ref.py
+++ wk4/ref.py
@@ -10,16 +10,32 @@ def mutate(entry):
     print(entry, "in func")
 
 
-# def what_string():
-#     value = "Hello"
-#     print("init", value)
-#     mutate_string(value)
-#     print("after", value)
+def what_string():
+    value = "Hello"
+    print("init", value)
+    val2 = mutate_string(value)
+    print("after", value, val2)
 
 
-# def mutate_string(entry: str):
-#     entry += " World"
-#     print("in func", entry)
+def mutate_string(entry: str):
+    entry += " World"
+    print("in func", entry)
+    return entry
 
 
-main()
+def bool_check():
+    init = True
+    print("init", init)
+    init = bool_mutate(init)
+    print("after", init)
+
+
+def bool_mutate(entry):
+    entry = False
+    print("in func", entry)
+    return entry
+
+
+# main()
+# what_string()
+bool_check()
